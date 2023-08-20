5047daf13ccccbb2569963d6bf22a1fa4ad48185
heehae <hhdev1117@gmail.com>
2023-08-20 17:48:28 +0900

    토비스프링부트 강의

---
+++ src/test/java/com/project/tobyspringboot/TobySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tobyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringBootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
